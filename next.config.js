2bd8da426bf0467233762798adce5811b48f27f6
woohee Byun <cay107@naver.com>
2023-10-05 00:51:28 +0900

    [Edit] dispoly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   styledComponents: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  assetPrefix: "/nextjs13.3",
 };
 
 module.exports = nextConfig;
